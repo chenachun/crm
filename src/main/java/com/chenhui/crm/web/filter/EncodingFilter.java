e3be15240f3accb34005f0ff839dc46936d234ca
chenhui <chenhui@2043600393.com>
2020-10-06 09:59:04 +0800

    CRM提交20201006

--- src/main/java/com/chenhui/crm/web/filter/EncodingFilter.java
+++ src/main/java/com/chenhui/crm/web/filter/EncodingFilter.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class EncodingFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         servletRequest.setCharacterEncoding("UTF-8");
-        servletResponse.setContentType("text/html;charsetsutf-8");
+        servletResponse.setContentType("text/html;charset=utf-8");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
