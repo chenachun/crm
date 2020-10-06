366ca4e40f09109eef5a4feb1dadb6139f0f1027
chenhui <chenhui@2043600393.com>
2020-10-06 08:40:30 +0800

    CRM提交20201006

---
+++ src/main/java/com/chenhui/crm/settings/exception/LoginException.java
@@ -0,0 +1,14 @@
+package com.chenhui.crm.settings.exception;
+
+/**
+ * ClassName : LoginException
+ * Package : com.chenhui.crm.settings.exception
+ * Description :
+ *
+ * @author : chenhui@xvco.com
+ */
+public class LoginException extends Exception{
+    public LoginException(String message) {
+        super(message);
+    }
+}
